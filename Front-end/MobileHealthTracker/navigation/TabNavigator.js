56168097ba5e5f3d814445f2422e46647ca79888
AlcoRV <zennerqwert@ya.ru>
2023-02-10 03:30:10 +0300

    adding main screen and avatar svg

--- Front-end/MobileHealthTracker/navigation/TabNavigator.js
+++ Front-end/MobileHealthTracker/navigation/TabNavigator.js
@@ -1,17 +1,21 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
 import AuthorizationScreen from "../src/screens/AuthorizationScreen";
-import { TestScreen } from "../src/screens/TestScreen";
+import MainScreen from "../src/screens/MainScreen";
+import TestScreen from "../src/screens/TestScreen";
 
 const Tab = createBottomTabNavigator();
 
-export const TabNavigator = () => {
+const TabNavigator = () => {
     return(
             <Tab.Navigator screenOptions={{
                 headerShown: false
             }}>
                 <Tab.Screen name="Test" component={TestScreen} />
+                <Tab.Screen name="Main" component={MainScreen} />
                 <Tab.Screen name="Auth" component={AuthorizationScreen} />
             </Tab.Navigator>
     );
 };
+
+export default TabNavigator;
---
+++ Front-end/MobileHealthTracker/src/screens/MainScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+
+const MainScreen = () => {
+    return(
+        <SafeAreaView style={styles.container}>
+ 
+        </SafeAreaView>
+    );
+}
+
+export default MainScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        height: '100%',
+        width: '100%',
+        backgroundColor: 'red',
+    },
+  });
