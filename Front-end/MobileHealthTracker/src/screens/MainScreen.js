e797f753f51d08afdd90df66d91f97abf763821e
AlcoRV <zennerqwert@ya.ru>
2023-02-14 10:39:56 +0300

    adding avatar

--- Front-end/MobileHealthTracker/src/screens/MainScreen.js
+++ Front-end/MobileHealthTracker/src/screens/MainScreen.js
@@ -1,10 +1,12 @@
 import React from "react";
-import { SafeAreaView, StatusBar, StyleSheet, Text, View } from "react-native";
+import { Image, SafeAreaView, StyleSheet, View } from "react-native";
 
 const MainScreen = () => {
     return(
         <SafeAreaView style={styles.container}>
- 
+            <View style={styles.avatar}>
+                <Image height={100} width={100} source={require('../../images/profile.png')} />
+            </View>
         </SafeAreaView>
     );
 }
@@ -16,6 +18,11 @@ const styles = StyleSheet.create({
         flex: 1,
         height: '100%',
         width: '100%',
-        backgroundColor: 'red',
+        //backgroundColor: 'red',
     },
+    avatar: {
+        alignItems: "flex-end",
+        paddingRight: 20,
+        paddingTop: 40
+    }
   });
