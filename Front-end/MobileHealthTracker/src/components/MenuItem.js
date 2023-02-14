a20e406b22c9308da25b35b0351f5750ceb6f5ac
AlcoRV <zennerqwert@ya.ru>
2023-02-14 21:40:37 +0300

    created menu item

---
+++ Front-end/MobileHealthTracker/src/components/MenuItem.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+
+
+
+export default function MenuItem(props){
+   
+    return(
+        <LinearGradient style={styles.menuItem} colors={['#1A94D6', '#1CB0FF', '#029EF2']}>
+            <Text style={styles.menuText}>{props.children}</Text>
+        </LinearGradient>
+    );
+};
+
+const styles = StyleSheet.create({
+    menuItem:{
+        borderColor: 'black',
+        borderWidth: 1,
+        flex: 1
+    },
+    menuText: {
+        fontSize: 24,
+        padding: 10,
+        fontWeight: "600"
+    }
+  });
