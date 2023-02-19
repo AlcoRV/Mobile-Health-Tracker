4d7216f1ecf7a2289c622f937932eb456ccfc1e4
AlcoRV <zennerqwert@ya.ru>
2023-02-20 00:51:33 +0300

    optimized tabNavigator

--- Front-end/MobileHealthTracker/navigation/TabNavigator.js
+++ Front-end/MobileHealthTracker/navigation/TabNavigator.js
@@ -1,40 +1,44 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import React from "react";
-import AuthorizationScreen from "../src/screens/AuthorizationScreen";
 import MainScreen from "../src/screens/MainScreen";
 import TestScreen from "../src/screens/TestScreen";
 import Ionic from "react-native-vector-icons/Ionicons";
 
 const Tab = createBottomTabNavigator();
 
+const itemOptions = {
+  MainMenu: {
+    title: 'Главное меню',
+    activeIconName: 'home',
+    inactiveIconName: 'home-outline'
+  },
+  MedCard: {
+    title: 'Мед. карточка',
+    activeIconName: 'person',
+    inactiveIconName: 'person-outline'
+  }
+};
+
 const TabNavigator = () => {
     return(
             <Tab.Navigator 
             screenOptions={({ route }) => ({
                 tabBarIcon: ({ focused, color, size }) => {
-                  let iconName;
-                  if (route.name === 'Main') {
-                    iconName = focused ? 'home' : 'home-outline';
-                  } else if (route.name === 'Test') {
-                    iconName = focused ? 'person' : 'person-outline';
-                  } else if (route.name === 'Auth') {
-                    iconName = focused ? 'person' : 'person-outline';
-                  }
-        
+                  let iconName = focused ? itemOptions[route.name].activeIconName : itemOptions[route.name].inactiveIconName;  
                   return <Ionic name={iconName} size={size} color={color} />;
                 },
                 headerShown: false,
                 tabBarStyle: {
                     backgroundColor: '#535AF5'
-                }
+                },
+                title: itemOptions[route.name].title,
               })} 
               tabBarOptions={{
                 activeTintColor: '#F0CB35',
                 inactiveTintColor: '#F5D34C',
               }}>
-                <Tab.Screen name="Test" component={TestScreen} />
-                <Tab.Screen name="Main" component={MainScreen} />
-                <Tab.Screen name="Auth" component={AuthorizationScreen} />
+                <Tab.Screen name="MainMenu" component={MainScreen} />
+                <Tab.Screen name="MedCard" component={TestScreen} />
             </Tab.Navigator>
     );
 };
