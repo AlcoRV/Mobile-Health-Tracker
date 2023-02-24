52f82e6616e074298001fb3dd086713e92dc4177
AlcoRV <zennerqwert@ya.ru>
2023-02-24 23:23:02 +0300

    added first parameters in health screen

--- Front-end/MobileHealthTracker/src/screens/HealthScreen.js
+++ Front-end/MobileHealthTracker/src/screens/HealthScreen.js
@@ -1,13 +1,27 @@
 import React from "react";
+import { StyleSheet, View } from "react-native";
 import BackButton from "../components/BackButton";
+import FormSection from "../components/FormSection";
+import { InfoLineParameter } from "../components/InfoLine";
 import MainTemplateBkg from "../components/MainTemplateBkg";
 
 const HealthScreen = () => {
     return(
         <MainTemplateBkg>
             <BackButton/>
+            <FormSection>
+                <View style={{flexDirection: "row", margin: 20, backgroundColor: 'red'}}>
+                    <InfoLineParameter paramName='Вес' scaleName='кг.' maxLength={5} />
+                    <InfoLineParameter paramName='Рост' scaleName='м.' maxLength={4} />
+                </View>
+                
+            </FormSection>
         </MainTemplateBkg>
     );
 };
 
 export default HealthScreen;
+
+const styles = StyleSheet.create({
+    
+});
