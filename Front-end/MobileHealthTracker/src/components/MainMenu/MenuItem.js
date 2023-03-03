590c7f9477ac5e676f24f60d304ecb5efbc583a9
AlcoRV <zennerqwert@ya.ru>
2023-03-03 10:32:10 +0300

    started design medicines screen

--- Front-end/MobileHealthTracker/src/components/MainMenu/MenuItem.js
+++ Front-end/MobileHealthTracker/src/components/MainMenu/MenuItem.js
@@ -4,12 +4,12 @@ import { LinearGradient } from "expo-linear-gradient";
 
 
 
-export default function MenuItem({children, onPress}){
+export default function MenuItem({children, onPress, style}){
    
     onPress = onPress ? onPress : () => {};
 
     return(
-        <LinearGradient style={styles.menuItem} colors={['#1A94D6', '#1CB0FF', '#029EF2']}>
+        <LinearGradient style={[styles.menuItem, style]} colors={['#1A94D6', '#1CB0FF', '#029EF2']}>
             <TouchableOpacity onPress={ onPress } style={{ flex: 1}}>
                 <Text style={styles.menuText}>{children}</Text>
             </TouchableOpacity>
--- Front-end/MobileHealthTracker/src/screens/MedicinesScreen.js
+++ Front-end/MobileHealthTracker/src/screens/MedicinesScreen.js
@@ -1,15 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet } from "react-native";
 import FormSection from "../components/FormSection";
+import MenuItem from "../components/MainMenu/MenuItem";
 import MainTemplateBkg from "../components/MainTemplateBkg";
 
+const testMedicines = [
+    {
+        name: 'Лекарство1',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство2',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство3',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство4',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство5',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство6',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство7',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство8',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство9',
+        description: 'Принимай столько и по столько'
+    },
+    {
+        name: 'Лекарство10',
+        description: 'Принимай столько и по столько'
+    }
+]
+
 const MedicinesScreen = () => {
+    const [listMedicines, setListMedicines] = useState(testMedicines);
+
     return(
         <MainTemplateBkg>
             <FormSection>
-
+                {
+                    listMedicines.map(item => (
+                        <MenuItem style={styles.item}>{item.name}</MenuItem>
+                    ))
+                }
             </FormSection>
         </MainTemplateBkg>
     );
 };
 
 export default MedicinesScreen;
+
+const styles = StyleSheet.create({
+    item: {
+        height: 100
+    }
+});
