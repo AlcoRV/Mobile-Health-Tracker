15e1a8d51d4db2d398435937b9cbf23282ee4885
AlcoRV <zennerqwert@ya.ru>
2023-03-04 13:22:51 +0300

    modified back button

--- Front-end/MobileHealthTracker/src/components/BackButton.js
+++ Front-end/MobileHealthTracker/src/components/BackButton.js
@@ -2,13 +2,15 @@ import { useNavigation } from "@react-navigation/native";
 import React from "react";
 import { Image, StyleSheet, TouchableOpacity, View } from "react-native";
 
-export default function BackButton(){
+export default function BackButton({onPress}){
 
     const navigation = useNavigation();
 
+    onPress = onPress || navigation.goBack;
+
     return(
         <View style={{flex: 1}}>
-            <TouchableOpacity onPress={ () => navigation.goBack() }>
+            <TouchableOpacity onPress={ () => onPress() }>
                 <Image source={require('../../images/goBack.png')} style={styles.goBack}/>
             </TouchableOpacity>
         </View>
