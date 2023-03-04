37f6e5ddd7fd9876bbf8287a181af5490b22e57f
AlcoRV <zennerqwert@ya.ru>
2023-03-04 10:49:56 +0300

    made more customable menu item

--- Front-end/MobileHealthTracker/src/components/MainMenu/MenuItem.js
+++ Front-end/MobileHealthTracker/src/components/MainMenu/MenuItem.js
@@ -4,12 +4,13 @@ import { LinearGradient } from "expo-linear-gradient";
 
 
 
-export default function MenuItem({children, onPress, style}){
+export default function MenuItem({children, onPress, style, colors}){
    
     onPress = onPress ? onPress : () => {};
+    colors = colors ? colors : ['#1A94D6', '#1CB0FF', '#029EF2'];
 
     return(
-        <LinearGradient style={[styles.menuItem, style]} colors={['#1A94D6', '#1CB0FF', '#029EF2']}>
+        <LinearGradient style={[styles.menuItem, style]} colors={colors}>
             <TouchableOpacity onPress={ onPress } style={{ flex: 1}}>
                 <Text style={styles.menuText}>{children}</Text>
             </TouchableOpacity>
@@ -21,7 +22,8 @@ const styles = StyleSheet.create({
     menuItem:{
         borderColor: 'black',
         borderWidth: 1,
-        flex: 1
+        flex: 1,
+        borderRadius: 5
     },
     menuText: {
         fontSize: 24,
