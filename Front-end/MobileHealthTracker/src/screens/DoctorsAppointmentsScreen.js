6ee7b0541dbada3df1cee0ab2b88a4eb79e75474
AlcoRV <zennerqwert@ya.ru>
2023-03-23 03:56:03 +0300

    work for doctors appointment screen

--- Front-end/MobileHealthTracker/src/screens/DoctorsAppointmentsScreen.js
+++ Front-end/MobileHealthTracker/src/screens/DoctorsAppointmentsScreen.js
@@ -8,7 +8,7 @@ import 'intl/locale-data/jsonp/ru';
 import DocktorsAppointment from "../../models/DoctorsAppointment";
 import { InfoLineParameterList } from "../components/InfoLine";
 import MenuItem from "../components/MainMenu/MenuItem";
-//import ComboBox from 'react-native-combobox';
+import DateHelper from "../components/options/DateHelper";
 
 const testAppointments = [
     {
@@ -28,6 +28,14 @@ const testAppointments = [
         date: new Date()
     }
 ]
+
+const testValues = {
+    doctors: ["Петров П.П.", "Филиппов Ф.Ф.", 'Сидоров С.С.'],
+    specializations: ['Терапевт', 'Хирург', 'Стоматолог'],
+    clinics: ["Клиника №1", "Клиника №2", "Клиника №3"],
+    dates: [new Date('2023-03-18'), new Date('2023-03-17'), new Date('2022-03-19')],
+    times: [new Date("0-0-0T10:30"), new Date("0-0-0T11:30:00.000Z"), new Date(10*40)]
+}
 
 const getFullFormatDate = date => {
 
@@ -66,19 +74,30 @@ const AppointmentItem = ({item, onPress}) => {
 const DoctorsAppointmentsScreen = () => {
     const [appointments, setAppointments] = useState(testAppointments);
     const [activeItem, setActiveItem] = useState(null);
+    const [editable, setEditable] = useState(false);
 
     const AppointmentFormItem = () => {    
         const visible = (Boolean)(activeItem);
-        console.log(activeItem);
+        const [clinic, setClinic] = useState(activeItem?.doctor?.clinic);
+        const [specialization, setSpecialization] = useState(activeItem?.doctor?.specialization);
+        const [doctor, setDoctor] = useState(activeItem?.doctor?.name);
+        const [date, setDate] = useState(new DateHelper().getDate(activeItem?.date));
+        const [time, setTime] = useState(new DateHelper().getTime(activeItem?.date));
 
-        return(
+        return( 
             <Modal visible={visible}>
                 <MainTemplateBkg>
                     <FormSection buttonOnPress={() => setActiveItem(null)} style={{ justifyContent: "center" }}>
-                        <InfoLineParameterList paramName={"Клиника"} paramValue={activeItem?.doctor?.clinic} />
-                        <InfoLineParameterList paramName={"Специализация"} paramValue={activeItem?.doctor?.specialization} />
-                        <InfoLineParameterList paramName={"Врач"} paramValue={activeItem?.doctor?.name} />
-                        {!(activeItem?.doctor?.clinic) && <MenuItem style={{ margin: 20, flex: 0, height: 60}} colors={['#9C5800', '#E88A10']} >Выбрать дату и время</MenuItem> }
+                        <InfoLineParameterList 
+                            paramName={"Клиника"} paramValue={clinic} setParamValue={setClinic} values={testValues.clinics} editable={editable}/>
+                        <InfoLineParameterList 
+                            paramName={"Специализация"} paramValue={specialization} setParamValue={setSpecialization} values={testValues.specializations} editable={editable} />
+                        <InfoLineParameterList 
+                            paramName={"Врач"} paramValue={doctor} setParamValue={setDoctor} values={testValues.doctors} editable={editable} />
+                        <InfoLineParameterList 
+                            paramName={"День"} paramValue={date} setParamValue={setDate} values={testValues.dates.map(d => new DateHelper().getDate(d))} editable={editable} />
+                        <InfoLineParameterList 
+                            paramName={"Время"} paramValue={time} setParamValue={setTime} values={testValues.times.map(t => new DateHelper().getTime(t))} editable={editable} />
                     </FormSection>
                 </MainTemplateBkg>
             </Modal>
@@ -90,10 +109,16 @@ const DoctorsAppointmentsScreen = () => {
             <FormSection>
                 <AppointmentFormItem />
                 <FlatList style={styles.list} data={appointments} renderItem={({item}) => (
-                            <AppointmentItem item={item} onPress={() => setActiveItem(item)} />
+                            <AppointmentItem item={item} onPress={() => {
+                                setActiveItem(new DocktorsAppointment(item));
+                                setEditable(false);
+                            }} />
                     )}  />
                     <View style={{}}>
-                        <TouchableOpacity onPress={ () => setActiveItem(new DocktorsAppointment()) }>
+                        <TouchableOpacity onPress={ () => {
+                                setActiveItem(new DocktorsAppointment());
+                                setEditable(true);
+                            }}>
                             <Image source={require('../../images/addButton.png')} style={styles.addButton}/>
                         </TouchableOpacity>
                     </View>
