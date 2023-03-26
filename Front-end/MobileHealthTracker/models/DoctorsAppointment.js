7a089d1485785ade15fe48b780d78ab8af82537d
AlcoRV <zennerqwert@ya.ru>
2023-03-26 17:51:40 +0300

    fixed appointment class

--- Front-end/MobileHealthTracker/models/DoctorsAppointment.js
+++ Front-end/MobileHealthTracker/models/DoctorsAppointment.js
@@ -2,14 +2,19 @@ import Doctor from "./Doctor";
 
 class DocktorsAppointment{
     constructor(appointment){
-        console.log(appointment);
         if(appointment){ 
             const {doctor, date} = appointment;
-            this.doctor = doctor || new Doctor();
-            this.date = date || new Date();
+            this.doctor = new Doctor(doctor);
+            this.date = date;
         }
-        this.doctor = new Doctor();
-        this.date = new Date();
+        else{
+            this.doctor = new Doctor();
+            this.date = '';
+        }
+    }
+
+    isValid(){
+        return this.doctor.isValid() && this.date && this.date instanceof Date;
     }
 }
 
