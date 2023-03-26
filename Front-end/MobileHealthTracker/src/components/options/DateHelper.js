2f67e764fd6706ad58c8c852fc9fd3f85ed60f4b
AlcoRV <zennerqwert@ya.ru>
2023-03-26 17:52:24 +0300

    added method in date helper

--- Front-end/MobileHealthTracker/src/components/options/DateHelper.js
+++ Front-end/MobileHealthTracker/src/components/options/DateHelper.js
@@ -13,6 +13,19 @@ class DateHelper{
         const minutes = date.getMinutes();
         return `${hours}:${minutes}`;
     }
+    
+    getCommonDate(date, time){
+        if(!(typeof date === 'string' && typeof time === 'string')){ return ''; }
+        date = date.split('.');
+        date = new Date(`${date[2]}-0${date[1]}-${date[0]}T00:00:00.000Z`);
+
+        time = time.split(':');
+        time = new Date(`0001-01-01T${time[0]}:${time[1]}:00.000Z`)
+
+        date.setHours(time.getHours());
+        date.setMinutes(time.getMinutes());
+        return date;
+    }
 }
 
 export default DateHelper;
