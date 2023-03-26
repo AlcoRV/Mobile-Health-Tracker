32003a16eef4c033c90857012ab3deadeb527463
AlcoRV <zennerqwert@ya.ru>
2023-03-26 17:51:19 +0300

    fixed doctor class

--- Front-end/MobileHealthTracker/models/Doctor.js
+++ Front-end/MobileHealthTracker/models/Doctor.js
@@ -1,8 +1,19 @@
 class Doctor{
-    constructor(specialization, name, clinic){
-        this.specialization = specialization || '';
-        this.name = name || '';
-        this.clinic = clinic || '';
+    constructor(other){
+        if(other){
+            const {specialization, name, clinic} = other;
+            this.specialization = specialization;
+            this.name = name;
+            this.clinic = clinic;
+        }
+        else{
+            this.specialization = this.name = this.clinic = '';
+        }
+    }
+
+    isValid(){
+        return this.specialization && this.name && this.clinic && 
+        typeof this.specialization === 'string' && typeof this.name === 'string' && typeof this.clinic === 'string';
     }
 }
 
