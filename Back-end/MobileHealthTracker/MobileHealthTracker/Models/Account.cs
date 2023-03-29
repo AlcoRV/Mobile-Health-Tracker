867cf23c633735040dc75dd85060c81de2df2bdc
AlcoRV <zennerqwert@ya.ru>
2023-03-29 21:13:24 +0300

    created account model

---
+++ Back-end/MobileHealthTracker/MobileHealthTracker/Models/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace MobileHealthTracker.Models
+{
+    public class Account
+    {
+        public Guid Id { get; set; }
+        public string? Login { get; set; }
+        public string? Password { get; set; }
+    }
+}
