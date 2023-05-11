1bb99391ce46e2538f04dfd203ef1e668792dc5e
AlcoRV <zennerqwert@ya.ru>
2023-05-12 02:03:01 +0300

    added bar chart

--- Front-end/MobileHealthTracker/src/components/StatisticScreen.js
+++ Front-end/MobileHealthTracker/src/components/StatisticScreen.js
@@ -1,7 +1,15 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, View } from "react-native";
 import MainTemplateBkg from "./MainTemplateBkg";
 import BackButton from "./BackButton";
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph,
+    StackedBarChart
+  } from "react-native-chart-kit";
 
 const testData = [
     {
@@ -52,11 +60,53 @@ const testData = [
 
 const StatisticScreen = () => {
 
+    const chartConfig = {
+        //backgroundColor: '#f000f0',
+    backgroundGradientFrom: '#eff3ff',
+    backgroundGradientFromOpacity: 0.3,
+    backgroundGradientTo: '#efefef',
+    backgroundGradientToOpacity: 0,
+    decimalPlaces: 2,
+    color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
+    fillShadowGradient: "skyblue",
+    fillShadowGradientOpacity: 1,
+    labelColor: (opacity = 1) => `rgba(255,255,0, ${1})`,
+    style: {
+      borderRadius: 16,
+    },
+    propsForLabels:{
+        fontSize: 16,
+        },
+      };
+
+      const data = {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            data: [20, 45, 28, 80, 99, 43],
+            color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+            strokeWidth: 2 // optional
+          }
+        ],
+        legend: ["Rainy Days"] // optional
+      };
+
+      const screenWidth = Dimensions.get("window").width;
 
     return(
         <MainTemplateBkg>
             <BackButton />
-
+            
+            <View style={{flex: 4 }}>
+                <BarChart style={{borderRadius: 20}}
+                    data={data}
+                    width={screenWidth}
+                    height={500}
+                    //yAxisLabel="$"
+                    chartConfig={chartConfig}
+                    verticalLabelRotation={30}
+                    />
+            </View>
         </MainTemplateBkg>
     );
 }
